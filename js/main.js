26a40f8b4e26fc4531782dc64c1c5ab07057d87c
smashMineGame <aryanp@khanlabschool.org>
2018-02-27 20:49:56 -0800

    created progress bar with buttons

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$('#step1').click(function() {
+  $('.progress').css("width", "25%");
+});
+
+$('#step2').click(function() {
+  $('.progress').css("width", "50%");
+});
+
+$('#step3').click(function() {
+  $('.progress').css("width", "75%");
+});
+
+$('#step4').click(function() {
+  $('.progress').css("width", "100%");
+});
